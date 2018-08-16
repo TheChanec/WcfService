f38ece5f28d28606ec089bec3989e4badff44490
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-16 10:02:19 -0500

    examples

---
+++ WcfService/Interfaces/ISalesService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService.Models;
+
+namespace WcfService.Interfaces
+{
+    [ServiceContract]
+    interface ISalesService
+    {
+        [OperationContract]
+        bool InsertCustomer(Customer obj);
+
+        [OperationContract]
+        List<Customer> GetAllCustomer();
+
+        [OperationContract]
+        bool DeleteCustomer(int Cid);
+
+        [OperationContract]
+        bool UpdateCustomer(Customer obj);
+    }
+
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public int CustomerID;
+        [DataMember]
+        public string CustomerName;
+        [DataMember]
+        public string Address;
+        [DataMember]
+        public string EmailId;
+    }
+}
---
+++ WcfService/SalesService.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfService.Interfaces;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SalesService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SalesService.svc or SalesService.svc.cs at the Solution Explorer and start debugging.
+    public class SalesService : ISalesService
+    {
+        public bool DeleteCustomer(int Cid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertCustomer(Customer obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customer obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
